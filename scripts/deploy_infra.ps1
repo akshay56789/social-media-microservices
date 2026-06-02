0dc42993db2bb2a42b5016dc5527566640e16a7e
Akshay Bhisikar <130206125+akshay56789@users.noreply.github.com>
2026-06-02 14:15:53 +0530

    Create deploy_infra.ps1

---
+++ scripts/deploy_infra.ps1
@@ -0,0 +1,14 @@
+# Get first resource group
+$resourceGroup = az group list --query "[0].name" -o tsv
+
+Write-Host "Using Resource Group: $resourceGroup"
+
+$securePassword = Read-Host "Enter SQL Admin Password" -AsSecureString
+
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword)
+$sqlPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+az deployment group create `
+  --resource-group $resourceGroup `
+  --template-file .\infra\main.bicep `
+  --parameters sqlAdminPassword="$sqlPassword"
